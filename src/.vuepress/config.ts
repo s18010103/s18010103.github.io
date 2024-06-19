0c4abfd58eb54c31ca14ee3f03395e738205110a
s18010103 <s18010104@hoyu.edu.hk>
2024-06-19 20:16:18 +0800

    1

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -1,7 +1,12 @@
 import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
+import { viteBundler } from '@vuepress/bundler-vite'
 
 export default defineUserConfig({
+  bundler: viteBundler({
+    viteOptions: {},
+    vuePluginOptions: {},
+  }),
   base: "/",
   lang: "zh-CN",
   title: "皮蛋520号",
